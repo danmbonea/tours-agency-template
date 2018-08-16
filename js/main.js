4b81d895950848263e0f6c03a6efd30e8e260e6e
Dan Mbonea <noblesavage8969@gmail.com>
2018-08-16 10:15:26 +0300

    Added slider and assests to markup.

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Sidenav
+const sideNav = document.querySelector(".sidenav");
+M.Sidenav.init(sideNav, {});
+
+// Slider
+const slider = document.querySelector(".slider");
+M.Slider.init(slider, {
+  indicators: false,
+  height: 500,
+  transition: 500,
+  interval: 6000
+});
+
+// Autocomplete
+const ac = document.querySelector(".autocomplete");
+M.Autocomplete.init(ac, {
+  data: {
+    Aruba: null,
+    "Cancun Mexico": null,
+    Hawaii: null,
+    Florida: null,
+    Mombasa: null,
+    Zanzibar: null
+  }
+});
